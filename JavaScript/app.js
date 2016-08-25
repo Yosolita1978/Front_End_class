a3a2b483258169b2d9d00d8fe49c3e80ec63d2ff
Cristina Rodriguez <yosola@gmail.com>
2016-08-24 20:39:31 -0700

    All the materials and exs for the Front-End class

---
+++ JavaScript/app.js
@@ -0,0 +1,12 @@
+var learningJava = confirm(" Are you sure you want to learn JavaScript? ")
+
+if (learningJava) {
+    var userAnsewr = prompt(" Why?: ");
+} else {
+    alert("You will regret this!!");
+}
+
+if (userAnsewr === "I wanted"){
+    alert("And you deserved!!!");
+
+}
